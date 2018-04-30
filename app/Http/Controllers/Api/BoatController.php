35df244c2638c20a26068e5e93c6701ec37573bb
lidichamp <lidichamp@gmail.com>
2018-04-30 15:28:46 +0100

    fix api

--- app/Http/Controllers/Api/BoatController.php
+++ app/Http/Controllers/Api/BoatController.php
@@ -20,7 +20,7 @@ class BoatController extends Controller
 
 	public function get_by_home_jetty(Request $request, $home_jetty){
 		$boats = Boat::where('home_jetty', $home_jetty)->get();
-		return Response::json(Returns::ok($boats);
+		return Response::json(Returns::ok($boats));
 	}
 	public function get_by_operator(Request $request, $operator){
 		$boats = Boat::where('operator', $operator)->get();
